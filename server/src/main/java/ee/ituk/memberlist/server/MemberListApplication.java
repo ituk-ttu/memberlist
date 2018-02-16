170ac1c952f4929a07f3ce903aaf90812f5e9c8b
Johannes Kümmel <jkymmel@gmail.com>
2018-02-16 17:09:22 +0200

    init

---
+++ server/src/main/java/ee/ituk/memberlist/server/MemberListApplication.java
@@ -0,0 +1,12 @@
+package ee.ituk.memberlist.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemberListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemberListApplication.class, args);
+	}
+}
---
+++ server/src/test/java/ee/ituk/memberlist/server/MemberListApplicationTests.java
@@ -0,0 +1,16 @@
+package ee.ituk.memberlist.server;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MemberListApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
