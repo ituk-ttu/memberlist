84ab620424fa3126fbc5a508dc7882be03045637
Johannes Kümmel <jkymmel@gmail.com>
2018-02-19 23:44:54 +0200

    added list component (without any functionality)

---
+++ client/src/components/panel/List.vue
@@ -0,0 +1,37 @@
+<template lang="pug">
+  div.container
+    table.table
+      thead
+        tr
+          th(scope="col") ID
+          th(scope="col") Nimi
+          th(scope="col") Isikukood
+          th(scope="col") E-mail
+          th(scope="col") Staatus
+      tbody
+        tr(v-for="person in people")
+          td {{ person.id }}
+          td {{ person.name }}
+          td {{ person.idCode }}
+          td {{ person.email }}
+          td {{ person.status }}
+</template>
+
+<script>
+  export default {
+    name: 'List',
+    data () {
+      return {
+        people: [
+          {
+            id: 0,
+            name: 'Kelly Garrett',
+            idCode: '49510210324',
+            email: 'kelly@ituk.ee',
+            status: 'MEMBER'
+          }
+        ]
+      }
+    }
+  }
+</script>
