07503436d38a6c4128911a95e00147b7b50fecf8
Krõõt Grete Mänd <krmand@ttu.ee>
2018-02-26 17:47:39 +0200

    getterid ja settereid Memberile

--- server/src/main/java/ee/ituk/memberlist/server/member/Member.java
+++ server/src/main/java/ee/ituk/memberlist/server/member/Member.java
@@ -2,7 +2,6 @@ package ee.ituk.memberlist.server.member;
 
 import ee.ituk.memberlist.server.access.AccessCollection;
 import lombok.Data;
-import lombok.Generated;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -26,4 +25,72 @@ public class Member {
     private LocalDate dateOfJoining;
     private AccessCollection accessCollection;
 
+    public long getId() {
+        return id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPersoalCode() {
+        return persoalCode;
+    }
+
+    public void setPersoalCode(String persoalCode) {
+        this.persoalCode = persoalCode;
+    }
+
+    public String getStudentCode() {
+        return studentCode;
+    }
+
+    public void setStudentCode(String studentCode) {
+        this.studentCode = studentCode;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public MemberStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(MemberStatus status) {
+        this.status = status;
+    }
+
+    public String getCardNr() {
+        return cardNr;
+    }
+
+    public void setCardNr(String cardNr) {
+        this.cardNr = cardNr;
+    }
+
+    public String getPhoneNr() {
+        return phoneNr;
+    }
+
+    public void setPhoneNr(String phoneNr) {
+        this.phoneNr = phoneNr;
+    }
+
+    public LocalDate getDateOfJoining() {
+        return dateOfJoining;
+    }
+
+
+    public AccessCollection getAccessCollection() {
+        return accessCollection;
+    }
+
+    public void modifyAccessCollection() {
+       //TODO: mõelda mis oleks kõige prarem viis selle tegemiseks ja realiseerida
+    }
 }
