5197de14949a9645cc6376693180d635eb12d7e2
makalv <makalv@ttu.ee>
2018-03-05 20:50:09 +0200

    membercontroller update

---
+++ server/src/main/java/ee/ituk/memberlist/server/member/MemberController.java
@@ -0,0 +1,39 @@
+package ee.ituk.memberlist.server.member;
+
+
+import org.springframework.stereotype.Controller;
+
+import java.util.Collection;
+import java.util.HashMap;
+
+@Controller
+public class MemberController {
+    private HashMap<Long, Member> memberList;
+
+
+    public void addMember(Member member) {
+        memberList.put(member.getId(), Member.builder().id(member.getId())
+                                                       .name(member.getName())
+                                                       .personalCode(member.getPersonalCode())
+                                                       .studentCode(member.getStudentCode())
+                                                       .email(member.getEmail())
+                                                       .status(member.getStatus())
+                                                       .cardNr(member.getCardNr())
+                                                       .phoneNr(member.getPhoneNr())
+                                                       .dateOfJoining(member.getDateOfJoining())
+                                                       .accessCollection(member.getAccessCollection())
+                                                       .build());
+    }
+
+
+    public Collection<Member> getMemberList() {
+        return memberList.values();
+    }
+
+
+    public Member getMemberById(long id) {
+        return memberList.get(id);
+    }
+
+
+}
