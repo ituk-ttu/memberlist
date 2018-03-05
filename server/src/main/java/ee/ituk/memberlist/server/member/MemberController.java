229220219f12e1be886f12123940fc9161bdcef8
Krõõt Grete Mänd <krmand@ttu.ee>
2018-03-05 17:37:31 +0200

    added hsqldb dependencie, MemberController, MemberRepository, MemberService

---
+++ server/src/main/java/ee/ituk/memberlist/server/member/MemberController.java
@@ -0,0 +1,7 @@
+package ee.ituk.memberlist.server.member;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MemberController {
+}
---
+++ server/src/main/java/ee/ituk/memberlist/server/member/MemberRepository.java
@@ -0,0 +1,7 @@
+package ee.ituk.memberlist.server.member;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MemberRepository {
+}
