38c286c5427c16f3e06af3162941ea5591e1d745
Johannes Kümmel <jkymmel@gmail.com>
2018-04-17 13:33:23 +0300

    refactor

--- server/src/test/java/ee/ituk/memberlist/server/accesscollection/AccessCollectionControllerTests.java
+++ server/src/test/java/ee/ituk/memberlist/server/accesscollection/AccessCollectionControllerTests.java
@@ -1,7 +1,5 @@
-package ee.ituk.memberlist.server.accessCollectionTest;
+package ee.ituk.memberlist.server.accesscollection;
 
-import ee.ituk.memberlist.server.accesscollection.AccessCollection;
-import ee.ituk.memberlist.server.accesscollection.AccessCollectionController;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
